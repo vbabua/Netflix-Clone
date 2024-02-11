043565d90b3620441cbeabc70966559b85e87361
yoonpaix <babuvadakemu@gmail.com>
2024-02-11 06:45:01 +0000

    Adding detailed comments to urls.py

--- netflix_site/core/urls.py
+++ netflix_site/core/urls.py
@@ -1,9 +1,18 @@
+# Import the path function from django.urls to define url paths, and import the views module from the current package
 from django.urls import path
 from . import views
 
+# List of url patterns that Django will use to match browser URLs to the appropriate view function
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('login', views.login, name = 'login'),
-    path('signup', views.signup, name = 'signup'),
+    # Define a URL pattern for the root URL ('') which maps to the 'index' view. 
+    # When the site's root URL is accessed, Django will execute the views.index function.
+    path('', views.index, name='index'),
     
+    # Define a URL pattern for the 'login' path. 
+    # When "/login" is accessed, Django will execute the views.login function.
+    path('login', views.login, name='login'),
+    
+    # Define a URL pattern for the 'signup' path.
+    # When "/signup" is accessed, Django will execute the views.signup function.
+    path('signup', views.signup, name='signup'),
 ]
